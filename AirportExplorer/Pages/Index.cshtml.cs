272a9bb179795b51dc5e360623af57e4836bdf75
jesus-miguel6 <>
2018-11-20 23:55:20 -0600

    aeropuerto
    equipo:::::::::gustavo alejandro-jesus miguel

---
+++ AirportExplorer/Pages/Index.cshtml.cs
@@ -0,0 +1,143 @@
+﻿using CsvHelper.Configuration;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using GoogleApi;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Places.Details.Request;
+using GoogleApi.Entities.Places.Photos.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
+using GoogleApi.Entities.Places.AutoComplete.Request;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using CsvHelper;
+
+namespace AirportExplorer.Pages
+
+
+{
+    public class AirportDetail
+    {
+        public string FormattedAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Photo { get; set; }
+        public string PhotoCredit { get; set; }
+        public string Website { get; set; }
+    }
+    public class IndexModel : PageModel
+    {
+        public string MapboxAccessToken { get; }
+        public string GoogleApiKey { get; }
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+
+            MapboxAccessToken = configuration["Mapbox:AccessToken"];
+            GoogleApiKey = configuration["google:ApiKey"];
+        }
+        public async Task<IActionResult> OnGetAirportDetail(string name, double latitude, double longitude)
+        {
+            var airportDetail = new AirportDetail();
+
+            // Execute the search request
+            var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
+            {
+                Key = GoogleApiKey,
+                Name = name,
+                Location = new Location(latitude, longitude),
+                Radius = 1000
+            });
+
+            // If we did not get a good response, or the list of results are empty then get out of here
+            if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
+                return new BadRequestResult();
+
+            // Get the first result
+            var nearbyResult = searchResponse.Results.FirstOrDefault();
+            string placeId = nearbyResult.PlaceId;
+            string photoReference = nearbyResult.Photos?.FirstOrDefault()?.PhotoReference;
+            string photoCredit = nearbyResult.Photos?.FirstOrDefault()?.HtmlAttributions.FirstOrDefault();
+
+            // Execute the details request
+            var detailsResonse = await GooglePlaces.Details.QueryAsync(new PlacesDetailsRequest
+            {
+                Key = GoogleApiKey,
+                PlaceId = placeId
+            });
+
+            // If we did not get a good response then get out of here
+            if (!detailsResonse.Status.HasValue || detailsResonse.Status.Value != Status.Ok)
+                return new BadRequestResult();
+
+            // Set the details
+            var detailsResult = detailsResonse.Result;
+            airportDetail.FormattedAddress = detailsResult.FormattedAddress;
+            airportDetail.PhoneNumber = detailsResult.InternationalPhoneNumber;
+            airportDetail.Website = detailsResult.Website;
+
+            if (photoReference != null)
+            {
+                // Execute the photo request
+                var photosResponse = await GooglePlaces.Photos.QueryAsync(new PlacesPhotosRequest
+                {
+                    Key = GoogleApiKey,
+                    PhotoReference = photoReference,
+                    MaxWidth = 400
+                });
+
+                if (photosResponse.Buffer != null)
+                {
+                    airportDetail.Photo = Convert.ToBase64String(photosResponse.Buffer);
+                    airportDetail.PhotoCredit = photoCredit;
+                }
+            }
+
+            return new JsonResult(airportDetail);
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetAirports()
+        {
+            var configuration = new Configuration
+            {
+                BadDataFound = context => { }
+            };
+
+            using (var sr = new StreamReader(Path.Combine(_hostingEnvironment.WebRootPath, "airports.dat")))
+            using (var reader = new CsvReader(sr, configuration))
+            {
+                FeatureCollection featureCollection = new FeatureCollection();
+
+                while (reader.Read())
+                {
+                    string name = reader.GetField<string>(1);
+                    string iataCode = reader.GetField<string>(4);
+                    double latitude = reader.GetField<double>(6);
+                    double longitude = reader.GetField<double>(7);
+
+                    featureCollection.Features.Add(new Feature(
+                        new Point(new Position(latitude, longitude)),
+                        new Dictionary<string, object>
+                        {
+                    {"name", name},
+                    {"iataCode", iataCode}
+                        }));
+                }
+
+                return new JsonResult(featureCollection);
+            }
+        }
+    }
+}
